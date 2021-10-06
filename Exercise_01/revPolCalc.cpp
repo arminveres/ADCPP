2aebff1fcbaeb567479b6134af2e22b30b67d1d5
Armin Veres <armin.veres@ams.com>
2021-10-06 14:16:53 +0200

    added ex1.3 reverse polish notation calc

--- Exercise_01/revPolCalc.cpp
+++ Exercise_01/revPolCalc.cpp
@@ -1,90 +1,134 @@
 //
 // Created by arminveres on 10/5/21.
 //
-
-#include <iostream>
 #include <vector>
 #include <sstream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
+int main() 
+{
     vector<int> stack;
 
     auto curr = begin(stack);
 
     string cmd;
     bool isRunning = true;
-    bool cmdExists = true;
     char ch;
     int num;
 
     while(isRunning) {
         cout << "Enter command: " << endl;
         getline(cin, cmd);
-//        cout << cmd << endl;
+
         // holds commands
         istringstream argsStream(cmd);
 
-        while (cmdExists) {
+        while (argsStream >> ch) {
             if (!argsStream.good()) break;
-
-            argsStream >> ch;
-//            cout << ch;
 
             switch (ch) {
                 case 'n': {
                     argsStream >> num;
                     stack.push_back(num);
                     curr = end(stack) - 1;
-                    cout << *curr << endl;
-
+                    break;
+                }
+                case 'q': {
+                    argsStream >> ch;
+                    cout << "quitting..." << endl;
+                    isRunning = false;
+                    break;
+                }
+                case 'd': {
+                    if (curr >= begin(stack)) {
+                        argsStream >> ch;
+                        stack.pop_back();
+                        curr = end(stack) - 1;
+                    } else {
+                        cout << "nothing on the stack" << endl;
+                    }
                     break;
                 }
                 case '+': {
-                    if (curr != end(stack)) {
-//                        cout << "curr=" << *curr << endl;
+                    if (curr != begin(stack)) {
+						argsStream >> ch;
+						cout << "operation: " << ch << endl;
+
+                        stack.pop_back();  
+                        stack.pop_back();
                         int res = *curr + *(curr-1);
+						
                         stack.push_back(res);
                         curr = end(stack) - 1;
-                        cout << "the result: " << res << endl;
-//                        cmdExists = false;
+                        cout << "result: " << res << endl;
                         cout << "current iterator value: " << *curr << endl;
+                    } else {
+                        cout << "nothing on the stack" << endl;
                     }
                     break;
                 }
                 case '-': {
-                    if (curr != end(stack)) {
-                        int res = *curr - *(--curr);
+                    if (curr != begin(stack)) {
+						argsStream >> ch;
+						cout << "operation: " << ch << endl;
+                        stack.pop_back();  
+                        stack.pop_back();
+                        int res = *curr - *(curr-1);
+						
                         stack.push_back(res);
-                        cout << res << endl;
-
-//                        cout << *first << "\t" << *last << "\t" << *curr << endl;
+                        curr = end(stack) - 1;
+                        cout << "result: " << res << endl;
+                        cout << "current iterator value: " << *curr << endl;
+                    } else {
+                        cout << "nothing on the stack" << endl;
                     }
                     break;
                 }
                 case '*': {
+                    if (curr != begin(stack)) {
+						argsStream >> ch;
+						cout << "operation: " << ch << endl;
+
+                        stack.pop_back();  
+                        stack.pop_back();
+                        int res = *curr * *(curr-1);
+						
+                        stack.push_back(res);
+                        curr = end(stack) - 1;
+                        cout << "result: " << res << endl;
+                        cout << "current iterator value: " << *curr << endl;
+                    } else {
+                        cout << "nothing on the stack" << endl;
+                    }
                     break;
                 }
                 case '/': {
+                    if (curr != begin(stack)) {
+						argsStream >> ch;
+						cout << "operation: " << ch << endl;
+
+                        stack.pop_back();  
+                        stack.pop_back();
+                        int res = *curr / *(curr-1);
+						
+                        stack.push_back(res);
+                        curr = end(stack) - 1;
+                        cout << "result: " << res << endl;
+                        cout << "current iterator value: " << *curr << endl;
+                    } else {
+                        cout << "nothing on the stack" << endl;
+                    }
                     break;
                 }
-                case 'q': {
-                    cout << "quitting...";
-                    isRunning = false;
-                    cmdExists = false;
-                    break;
-                }
-                default: {
-                    cout << "invalid cmd" << endl;
-                    cmdExists = false;
-//                    isRunning = false;
-                    break;
-                }
-            }
+        	}
+        
+        if (isRunning) {
+            cout << "Stack:\t";
+            for (int st : stack) cout << st << "\t";
+            cout << endl;
         }
-        cout << "Stack:\t";
-        for (int st : stack) cout << st << "\t";
-        cout << endl;
+		}
     }
 }
